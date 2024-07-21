250f0bfe140ecd687e8f610597a814d72f4f0b1e
YoungJun Park <youngjun108059@gmail.com>
2024-07-22 00:16:20 +0900

    Fix: mysql r2dbc setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("io.r2dbc:r2dbc-mysql")
+	implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("mysql:mysql-connector-java")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	runtimeOnly("mysql:mysql-connector-java:8.0.28")
+	runtimeOnly("io.r2dbc:r2dbc-pool")
+	runtimeOnly("io.r2dbc:r2dbc-spi")
+	runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.80.Final:osx-aarch_64")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
