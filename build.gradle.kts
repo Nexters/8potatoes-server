7dbad79ed48e38e461a2899400a8178ca7979a81
YoungJun Park <youngjun108059@gmail.com>
2024-07-22 01:20:26 +0900

    Update: r2dbc dependency(cause error)
    `java.lang.IllegalArgumentException: Cannot encode value of type 'class io.r2dbc.spi.Parameters$InParameter'`
    
    https://github.com/spring-projects/spring-data-relational/issues/1573

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+	implementation("io.asyncer:r2dbc-mysql:1.1.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
