585b0609b726b8ce21585553511cfa95ef9e92c2
YoungJun Park <youngjun108059@gmail.com>
2024-07-22 00:20:38 +0900

    Update: docker-compose setting and init query

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -2,5 +2,18 @@ CREATE USER 'master'@'%' IDENTIFIED BY 'master';
 GRANT ALL PRIVILEGES ON *.* TO 'master'@'%';
 FLUSH PRIVILEGES;
 
---TODO: table setting
-drop table IF EXISTS test;
+CREATE DATABASE IF NOT EXISTS reststop;
+
+USE reststop;
+
+CREATE TABLE menu (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    food_seq VARCHAR(255),
+    price INT,
+    description TEXT,
+    is_recommended BOOLEAN,
+    is_premium BOOLEAN,
+    is_best_food BOOLEAN,
+    reststop_id INT
+);
