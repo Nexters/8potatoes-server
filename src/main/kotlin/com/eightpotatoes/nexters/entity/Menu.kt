cb7620417aae36a2bbad0e857a254bc6ae607bed
YoungJun Park <youngjun108059@gmail.com>
2024-07-22 00:22:23 +0900

    Update: menu data class

--- src/main/kotlin/com/eightpotatoes/nexters/entity/Menu.kt
+++ src/main/kotlin/com/eightpotatoes/nexters/entity/Menu.kt
@@ -6,8 +6,10 @@ import org.springframework.data.relational.core.mapping.Table
 @Table("Menu")
 data class Menu(
     @Id val id: Int,
+    val foodSeq: String,
     val name: String,
     val price: Int,
+    val description: String?,
     val isRecommended: Boolean,
     val isPremium: Boolean,
     val isBestFood: Boolean,
