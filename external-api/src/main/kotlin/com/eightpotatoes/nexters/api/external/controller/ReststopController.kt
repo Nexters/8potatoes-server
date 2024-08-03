34a16fdaebca0ac3f80c0772dd0358afe1d8f8db
jaejae-yoo <wotj102@gmail.com>
2024-08-03 16:42:49 +0900

    [feature/muilti-module-docker] setting docker for multi-module

--- external-api/src/main/kotlin/com/eightpotatoes/nexters/api/external/controller/ReststopController.kt
+++ external-api/src/main/kotlin/com/eightpotatoes/nexters/api/external/controller/ReststopController.kt
@@ -1,17 +1,21 @@
-//package com.eightpotatoes.nexters.api.external.controller
-//
-//import com.eightpotatoes.nexters.api.external.model.ReststopsAtHighway
-//import com.eightpotatoes.nexters.api.external.service.ReststopService
-//import org.springframework.web.bind.annotation.GetMapping
-//import org.springframework.web.bind.annotation.PathVariable
-//import org.springframework.web.bind.annotation.RequestParam
-//import org.springframework.web.bind.annotation.RestController
-//import reactor.core.publisher.Flux
-//import reactor.core.publisher.Mono
-//
-//@RestController
-//class ReststopController(private val reststopService: ReststopService) {
-//
+package com.eightpotatoes.nexters.api.external.controller
+
+import com.eightpotatoes.nexters.api.external.model.ReststopsAtHighway
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@RestController
+class ReststopController() {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello"
+    }
+
 //    @GetMapping("/api/highways/reststops")
 //    fun getReststops(
 //        @RequestParam from: String,
@@ -27,4 +31,4 @@
 //    ): Mono<ReststopDetails> {
 //        return reststopService.getReststopDetails(reststopCode)
 //    }
-//}
+}
