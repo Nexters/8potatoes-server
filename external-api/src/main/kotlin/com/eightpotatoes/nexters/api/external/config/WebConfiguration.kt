8fe2ebc5a8cdcbcb204740e900797a3b5f5edd72
YoungJun Park <youngjun108059@gmail.com>
2024-08-15 18:32:39 +0900

    [feature/ci-cd] fix: build error

--- external-api/src/main/kotlin/com/eightpotatoes/nexters/api/external/config/WebConfiguration.kt
+++ external-api/src/main/kotlin/com/eightpotatoes/nexters/api/external/config/WebConfiguration.kt
@@ -1,4 +1,4 @@
-package com.eightpotatoes.nexters.core.config
+package com.eightpotatoes.nexters.api.external.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
